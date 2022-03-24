04d53c682bb1d6f972fe169efc2356c592573e17
TRipura <tripurakant@gmail.com>
2022-03-25 01:33:41 +0530

    tripura

---
+++ file_utilization.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+emailID="tripurakant@gmail.com"
+
+#It will filter the filesystem then print the % ultization and then tr (translate command to delete / sed 's/%//')
+FU=$(df -h | grep sda5 | awk '{ print $5 }' | tr -d "%")
+
+if [[ $FU -ge 60 ]]
+
+then
+	echo "The files system usage of sda5 is : $FU"
+fi
+
+
