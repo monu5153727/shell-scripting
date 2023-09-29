a24ccd567755c61f9e4f215028328150bb37ac92
Tripura kant <tripura.kant@zoom.us>
2023-09-30 00:13:30 +0530

    30sep

--- sample-scripts/4.sh
+++ sample-scripts/4.sh
@@ -8,3 +8,12 @@ echo "${myShellScr}" | tr '{a-z}' '{A-Z}'
 echo "${myShellScr}" | tr '{A-Z}' '{a-z}'
 
 
+sed -i 2 '2, 10d' myfiles.txt
+
+## tr 
+
+sed -i 2 '11, 110d' input.txt
+echo "Hi how are you " | tr '[A-Z]' '[a-z]'
+echo "Hi how are you " | tr '[a-z]' '[A-Z]'
+java --version | awk 'NR==2' | cut -d " " -f 6 | tr -d ')'
+brew services restart jenkins
