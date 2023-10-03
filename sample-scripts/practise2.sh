26796a9ea6bd1c4a4545158f277cd33ffd70ec92
Tripura kant <tripura.kant@zoom.us>
2023-10-03 23:59:11 +0530

    ok

--- sample-scripts/practise2.sh
+++ sample-scripts/practise2.sh
@@ -2,9 +2,9 @@
 
 pkgname=${1}
 
-
-
-if command -v ${pkgname} 1>/dev/null 2 >&1 ;
-
-brew remove  ${pkgname}
-brew install  ${pkgname}
+if command -v ${pkgname} 1>/dev/null 2 >&1 ; then
+  echo "Your package ${pkgname} is already deployed"
+else
+    brew remove  ${pkgname}
+    brew install  ${pkgname}
+fi
