7aedf429340ab6345ed305af3a781cf3c9a18ffe
Tripura kant <tripura.kant@zoom.us>
2023-10-15 12:40:06 +0530

    Automated commit at Sun Oct 15 12:40:06 IST 2023

--- sample-scripts/7.sh
+++ sample-scripts/7.sh
@@ -1,2 +1,8 @@
 # Script to get version details of tools
 
+#!/bin/bash
+
+ansibleVer=$( ansible --version | awk 'NR==1' | cut -d ' ' -f3 | tr -d ']')
+
+echo "ToolName      VersionInfo"
+echo "Ansible       ${ansibleVer}"
